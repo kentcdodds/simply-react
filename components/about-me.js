0b0591e1d777b5719af77ba220cfa4ca7539a5b8
Kent C. Dodds <kent@doddsfamily.us>
2018-08-05 19:31:44 -0600

    more progress

--- components/about-me.js
+++ components/about-me.js
@@ -6,45 +6,112 @@ const Container = styled.div`
   justify-content: space-around;
 `
 
+const IconImage = styled.img`
+  max-height: 70px;
+  max-width: 70px;
+`
+
+function LinkedIconImage({link, name}) {
+  return (
+    <a href={link} target="_blank">
+      <IconImage src={`public/standard/${name}`} />
+    </a>
+  )
+}
+
 const IconImageContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
 `
 
-const IconImage = styled.img`
-  max-height: 70px;
-  max-width: 70px;
-`
-
 const Family = styled.div`
   display: flex;
+`
+
+const LogoRow = styled(Container)`
+  margin-top: 30px;
+  margin-bottom: 30px;
 `
 
 class AboutMe extends React.Component {
   render() {
     return (
-      <Container>
-        <IconImageContainer>
-          <IconImage src="public/standard/house.png" />
-          <span>Utah</span>
-        </IconImageContainer>
-        <IconImageContainer>
-          <Family>
-            <IconImage src="public/standard/woman.png" />
-            <IconImage src="public/standard/girl.png" />
-            <IconImage src="public/standard/boy.png" />
-            <IconImage src="public/standard/boy.png" />
-            <IconImage src="public/standard/boy.png" />
-            <IconImage src="public/standard/dog.png" />
-          </Family>
-          <span>wife, 4 kids, & a dog</span>
-        </IconImageContainer>
-        <IconImageContainer>
-          <IconImage src="public/standard/office.png" />
-          <span>PayPal</span>
-        </IconImageContainer>
-      </Container>
+      <div>
+        <Container>
+          <IconImageContainer>
+            <LinkedIconImage
+              link="https://www.google.com/maps/place/Utah"
+              name="house.png"
+            />
+            <span>Utah</span>
+          </IconImageContainer>
+          <IconImageContainer>
+            <Family>
+              <LinkedIconImage
+                link="https://www.lds.org/topics/family-proclamation"
+                name="woman.png"
+              />
+              <LinkedIconImage
+                link="https://www.lds.org/topics/family-proclamation"
+                name="girl.png"
+              />
+              <LinkedIconImage
+                link="https://www.lds.org/topics/family-proclamation"
+                name="boy.png"
+              />
+              <LinkedIconImage
+                link="https://www.lds.org/topics/family-proclamation"
+                name="boy.png"
+              />
+              <LinkedIconImage
+                link="https://www.lds.org/topics/family-proclamation"
+                name="boy.png"
+              />
+              <LinkedIconImage
+                link="https://photos.google.com/search/_cAF1QipNRwO3NP-6XsiM36GnFIhCiZW9nB6UFB60_Gemma/photo/AF1QipNUOABnMsdf4cEXs-D32qIIyHmrbTYT2FBIoa1j"
+                name="dog.png"
+              />
+            </Family>
+            <span>wife, 4 kids, & a dog</span>
+          </IconImageContainer>
+          <IconImageContainer>
+            <LinkedIconImage
+              link="https://goo.gl/maps/WBspiX9s6zz"
+              name="office.png"
+            />
+            <span>PayPal</span>
+          </IconImageContainer>
+        </Container>
+        <LogoRow>
+          <LinkedIconImage
+            link="https://github.com/kentcdodds"
+            name="github.png"
+          />
+          <LinkedIconImage link="http://kcd.im/news" name="kcd-news.png" />
+          <LinkedIconImage
+            link="https://blog.kentcdodds.com/"
+            name="medium.png"
+          />
+          <LinkedIconImage
+            link="https://kentcdodds.com/talks/"
+            name="speaking.png"
+          />
+          <LinkedIconImage
+            link="http://kcd.im/3-mins"
+            name="3-minutes-with-kent.png"
+          />
+          <LinkedIconImage link="http://kcd.im/ama" name="ama.png" />
+        </LogoRow>
+        <LogoRow>
+          <LinkedIconImage link="http://kcd.im/egghead" name="eggheadio.png" />
+          <LinkedIconImage link="http://kcd.im/fem" name="fem.png" />
+          <LinkedIconImage link="http://kcd.im/wme" name="workshopme.png" />
+          <LinkedIconImage link="http://kcd.im/twitch" name="twitch.png" />
+          <LinkedIconImage link="http://kcd.im/gde" name="gde.png" />
+          <LinkedIconImage link="http://kcd.im/youtube" name="youtube.png" />
+        </LogoRow>
+      </div>
     )
   }
 }
